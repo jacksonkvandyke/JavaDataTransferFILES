75319122be2e19e6a1a125707b018483ce017fc2
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-04 20:08:29 -0600

    Debug

--- GatherAllFiles.java
+++ GatherAllFiles.java
@@ -167,7 +167,7 @@ class OpenFile extends Thread{
             //Check if data can be added to stream
             if ((dataStream.size() < 1000) && (convertedFile.packetIterator < convertedFile.packets.length)){
                 //Add packet to dataStream
-                if (convertedFile.packets.length > 0){
+                if (convertedFile.packets[convertedFile.packetIterator] != null){
                     dataStream.add(convertedFile.GetNextPacket());
                 }
 
