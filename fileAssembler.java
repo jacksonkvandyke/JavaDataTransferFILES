30b0ae97fbd82ea638f286a0835d8b31c45a796b
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-08 16:17:31 -0600

    Test

--- fileAssembler.java
+++ fileAssembler.java
@@ -6,20 +6,22 @@ public class fileAssembler {
     String currentDirectory = null;
     File file = null;
 
-    synchronized void SavePacket(Packet packet, File file){
+    void SavePacket(Packet packet){
         //Create file if it hasn't already been created
 
         if (!file.exists()){
             try{
-                //Create file and write empty data
-                file = new File(packet.getFilename());
-                file.createNewFile();
-                System.out.println("File successfully created!");
+                synchronized(file){
+                    //Create file and write empty data
+                    file = new File(packet.getFilename());
+                    file.createNewFile();
+                    System.out.println("File successfully created!");
 
-                FileOutputStream currentWriter = new FileOutputStream(file.getAbsolutePath());
-                currentWriter.write(new byte[packet.getTotalPackets() * 1024]);
-                currentWriter.flush();
-                currentWriter.close();
+                    FileOutputStream currentWriter = new FileOutputStream(file.getAbsolutePath());
+                    currentWriter.write(new byte[packet.getTotalPackets() * 1024]);
+                    currentWriter.flush();
+                    currentWriter.close();
+                }
             }catch (IOException e){
                 System.out.print(e);
             }
