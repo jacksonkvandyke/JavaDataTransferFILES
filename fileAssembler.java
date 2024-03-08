250ad30d2633a98a9f3d0dc100ec49b4eddd58c0
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-08 17:04:30 -0600

    Test

--- fileAssembler.java
+++ fileAssembler.java
@@ -67,12 +67,11 @@ class SavePackets extends Thread{
                 //Write data to file
                 try{
                     FileOutputStream currentWriter = new FileOutputStream(file.getAbsolutePath());
-                    currentWriter.write(packet.getData(), packet.getSequence() * 1024, packet.getDataLength());
+                    currentWriter.write(packet.getData());
                     currentWriter.flush();
                     currentWriter.close();
                 }catch (IOException e){
                     System.out.print(e);
-                    System.out.print(packet.getSequence() * 1024);
                 }
             }
         }
