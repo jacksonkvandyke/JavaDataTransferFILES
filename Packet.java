c9f6541fd9923346bb91ce32973c47a38bb2bbf8
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-10 00:18:43 -0600

    Test

--- Packet.java
+++ Packet.java
@@ -3,23 +3,17 @@ import java.io.Serializable;
 public class Packet implements Serializable {
 
     private String fileName = "";
-    private long totalPackets = 0;
     private int packetSequence = 0;
     private byte packetData[];
 
-    public Packet(String fileName, long maxPackets, int sequence, byte data[]){
+    public Packet(String fileName, int sequence, byte data[]){
         this.fileName = fileName;
-        this.totalPackets = maxPackets;
         this.packetSequence = sequence;
         this.packetData = data;
     }
 
     public String getFilename() {
         return this.fileName;
-    }
-
-    public long getTotalPackets() {
-        return this.totalPackets;
     }
 
     public int getSequence() {
