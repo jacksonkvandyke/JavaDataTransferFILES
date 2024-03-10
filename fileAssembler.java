e43aa63d86ace6503ce567b833e3b248a3d29ca9
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-09 21:25:08 -0600

    Test

--- fileAssembler.java
+++ fileAssembler.java
@@ -53,7 +53,7 @@ class SavePackets extends Thread{
                 //Write data to file
                 try{
                     RandomAccessFile currentWriter = new RandomAccessFile(file.getAbsolutePath(), "rw");
-                    currentWriter.seek(packet.getSequence() * 10240);
+                    currentWriter.seek(packet.getSequence() * 1024);
                     currentWriter.write(packet.getData());
                     currentWriter.close();
                 }catch (IOException e){
