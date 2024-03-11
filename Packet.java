3de6b04e75dd56fbbc96d6d4fdcb100b810b44b5
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-10 22:02:17 -0500

    Test

--- Packet.java
+++ Packet.java
@@ -5,12 +5,14 @@ public class Packet implements Serializable {
     private String fileName = "";
     private int packetSequence = 0;
     private byte packetData[];
+    private long totalData = 0;
     private boolean isDirectory = false;
 
-    public Packet(String fileName, int sequence, byte data[]){
+    public Packet(String fileName, int sequence, byte data[], long totalData){
         this.fileName = fileName;
         this.packetSequence = sequence;
         this.packetData = data;
+        this.totalData = totalData;
     }
 
     public String getFilename() {
