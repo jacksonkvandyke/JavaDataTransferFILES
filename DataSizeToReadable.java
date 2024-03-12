69cac27153a7bf84f2899d27a58e91e91b015a80
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-11 23:06:30 -0500

    Test

--- DataSizeToReadable.java
+++ DataSizeToReadable.java
@@ -5,22 +5,22 @@ class DataSizeToReadable {
 
         //KB
         if ((data / 1024) > 1){
-            totalString = String.format("%s KB", String.valueOf(data / 1024));
+            totalString = String.format("%.2f KB", (data / 1024));
         }
 
         //MB
         if ((data / Math.pow(1024, 2)) > 1){
-            totalString = String.format("%s MB", String.valueOf(data / Math.pow(1024, 2)));
+            totalString = String.format("%.2f MB", data / Math.pow(1024, 2));
         }
 
         //GB
         if ((data / Math.pow(1024, 3)) > 1){
-            totalString = String.format("%s GB", String.valueOf(data / Math.pow(1024, 3)));
+            totalString = String.format("%.2f GB", data / Math.pow(1024, 3));
         }
 
         //TB
         if ((data / Math.pow(1024, 4)) > 1){
-            totalString = String.format("%s TB", String.valueOf(data / Math.pow(1024, 4)));
+            totalString = String.format("%.2f TB", data / Math.pow(1024, 4));
         }
 
         //Return final string
