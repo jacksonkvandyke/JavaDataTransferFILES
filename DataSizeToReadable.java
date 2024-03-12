d5b793069c5e4e8b965e45f518eb3732a967ba3c
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-11 23:00:37 -0500

    Test

--- DataSizeToReadable.java
+++ DataSizeToReadable.java
@@ -1,34 +1,26 @@
 class DataSizeToReadable {
 
-    long data = 0;
-
-    //This class converts data in number of bytes to the best readable format
-    DataSizeToReadable(long data){
-        this.data = data;
-        Convert();
-    }
-
-    String Convert(){
+    String Convert(long data){
         String totalString = "";
 
         //KB
         if ((data / 1024) > 1){
-            totalString = String.format("%d KB", data / 1024);
+            totalString = String.format("%d KB", (data / 1024));
         }
 
         //MB
         if ((data / Math.pow(1024, 2)) > 1){
-            totalString = String.format("%d MB", data / Math.pow(1024, 2));
+            totalString = String.format("%d MB", (data / Math.pow(1024, 2)));
         }
 
         //GB
         if ((data / Math.pow(1024, 3)) > 1){
-            totalString = String.format("%d GB", data / Math.pow(1024, 3));
+            totalString = String.format("%d GB", (data / Math.pow(1024, 3)));
         }
 
         //TB
         if ((data / Math.pow(1024, 4)) > 1){
-            totalString = String.format("%d TB", data / Math.pow(1024, 4));
+            totalString = String.format("%d TB", (data / Math.pow(1024, 4)));
         }
 
         //Return final string
