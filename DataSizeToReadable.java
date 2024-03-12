b1cd13598b1b69a2e953138a4195b60d6ff73e89
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-03-11 23:14:46 -0500

    Test

--- DataSizeToReadable.java
+++ DataSizeToReadable.java
@@ -5,22 +5,22 @@ class DataSizeToReadable {
 
         //KB
         if ((data / 1024) > 1){
-            totalString = String.format("%s KB", String.valueOf(Math.round(data / 1024 * 100) / 100));
+            totalString = String.format("%s KB", String.valueOf(Math.round((double) data / 1024 * 100) / 100));
         }
 
         //MB
         if ((data / Math.pow(1024, 2)) > 1){
-            totalString = String.format("%s MB", String.valueOf(Math.round(data / Math.pow(1024, 2) * 100) / 100));
+            totalString = String.format("%s MB", String.valueOf(Math.round((double) data / Math.pow(1024, 2) * 100) / 100));
         }
 
         //GB
         if ((data / Math.pow(1024, 3)) > 1){
-            totalString = String.format("%s GB", String.valueOf(Math.round(data / Math.pow(1024, 3) * 100) / 100));
+            totalString = String.format("%s GB", String.valueOf(Math.round((double) data / Math.pow(1024, 3) * 100) / 100));
         }
 
         //TB
         if ((data / Math.pow(1024, 4)) > 1){
-            totalString = String.format("%s TB", String.valueOf(Math.round(data / Math.pow(1024, 4) * 100) / 100));
+            totalString = String.format("%s TB", String.valueOf(Math.round((double) data / Math.pow(1024, 4) * 100) / 100));
         }
 
         //Return final string
